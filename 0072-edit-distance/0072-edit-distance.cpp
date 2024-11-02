7c46431460f2ca70ef7992537c3d93718a3a47ff
Prathamesh More <108971620+MPrathmesh@users.noreply.github.com>
2024-11-02 17:31:15 +0530

    Time: 6 ms (63.34%), Space: 13.4 MB (20.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -50,14 +50,48 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTabulation(string a, string b) {
+        //Step 1 - Create dp Array
+        vector<vector<int> >dp(a.length()+1, vector<int>(b.length()+1, -1));
+
+        //Step 2 - Analysis Base and case and insert the answer
+        for(int col = 0; col <= b.length(); col++) {
+            dp[a.length()][col] = b.length()-col;
+        }
+        for(int row = 0; row <= a.length(); row++) {
+            dp[row][b.length()] = a.length()-row;
+        }
+
+        for(int i_index = a.length()-1; i_index >= 0; i_index--) {
+            for(int j_index = b.length()-1; j_index >= 0; j_index--) {
+                int ans = 0;
+                if(a[i_index] == b[j_index]) {
+                    ans = 0 + dp[i_index+1][j_index+1];
+                }
+                else {
+                    int replace = 1 + dp[i_index+1][j_index+1];
+                    int insert = 1 + dp[i_index][j_index+1];
+                    int remove = 1 + dp[i_index+1][j_index];
+                    ans = min(insert, min(remove, replace));
+                }
+                dp[i_index][j_index] = ans;   
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int minDistance(string word1, string word2) {
         int i = 0;
         int j = 0;
         // int ans = solveUsingRecursion(word1, word2, i, j);
         // return ans;
 
-        vector<vector<int> > dp(word1.length()+1, vector<int>(word2.length()+1, -1));
-        int ans = solveUsingMem(word1, word2, i, j, dp);
+        // vector<vector<int> > dp(word1.length()+1, vector<int>(word2.length()+1, -1));
+        // int ans = solveUsingMem(word1, word2, i, j, dp);
+        // return ans;
+
+        int ans = solveUsingTabulation(word1, word2);
         return ans;
     }
 };
